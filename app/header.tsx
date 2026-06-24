6bcbd551c170f9d6b209d57acc4b519b7559ea02
TheKAI10 <ngorman189@gmail.com>
2026-06-24 18:22:06 -0400

    Made logo not have cursor-pointer since it isn't clickable

--- app/header.tsx
+++ app/header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
 
     return  <div className="sticky top-0 z-40 bg-zinc-950/80 backdrop-blur-md border-b border-zinc-900">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-18 flex items-center justify-between">
-                    <div className="flex items-center gap-3 cursor-pointer">
+                    <div className="flex items-center gap-3">
                         <img
                             src="/images/logo.png"
                             alt="Orlando Devs"
